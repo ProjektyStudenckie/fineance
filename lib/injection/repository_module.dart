20d32aa24acc4706401655d4af47c916c60fca75
Wojciech Wielochowski <45285800+Wielok@users.noreply.github.com>
2021-12-12 16:42:03 +0100

    add financial api

--- lib/injection/repository_module.dart
+++ lib/injection/repository_module.dart
@@ -3,6 +3,7 @@ import 'package:fineance/repositories/authentication_repository.dart';
 import 'package:fineance/repositories/settings_service.dart';
 import 'package:fineance/repositories/storage_repository.dart';
 import 'package:fineance/repositories/wallets_repository.dart';
+import 'package:fineance/repositories/yahoofin.dart';
 import 'package:get_it/get_it.dart';
 import 'package:local_auth/local_auth.dart';
 
@@ -10,5 +11,6 @@ void registerRepositoryModules(GetIt injector, LocalAuthentication localAuthenti
   injector.registerFactory<StorageService>(() => StorageServiceImpl(injector.get(instanceName: tokensBox)));
   injector.registerFactory<AuthenticationRepository>(() => AuthenticationRepositoryImpl(injector.get(), localAuthentication, injector.get()));
   injector.registerFactory<WalletRepository>(() => WalletRepository(injector.get(), injector.get()));
+  injector.registerFactory<YahooFinRepo>(() => YahooFinRepo());
   injector.registerFactory<SettingsService>(() => SettingsServiceImpl(injector.get(instanceName: settingsBox)));
 }
