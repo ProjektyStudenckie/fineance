4b74c6211c257a1d1cb28f488bf1e2b470a29f8d
Wojciech Wielochowski <45285800+Wielok@users.noreply.github.com>
2021-12-13 00:50:55 +0100

    Revert "change ip"
    This reverts commit 5b15f18370e64eaf971074cd8f88fd96c66db9fc.

--- lib/networking/api_client.g.dart
+++ lib/networking/api_client.g.dart
@@ -8,7 +8,7 @@ part of 'api_client.dart';
 
 class _ApiClient implements ApiClient {
   _ApiClient(this._dio, {this.baseUrl}) {
-    baseUrl ??= 'https://fineance.azurewebsites.net';
+    baseUrl ??= 'http://192.168.1.233:1332';
   }
 
   final Dio _dio;
