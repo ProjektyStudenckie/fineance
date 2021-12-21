5fe44d974784b5fc479589d3460c93b3cb82dec9
Wojciech Wielochowski <45285800+Wielok@users.noreply.github.com>
2021-12-21 22:01:23 +0100

    push basic implementation

--- lib/presentation/home/bloc/home_bloc.dart
+++ lib/presentation/home/bloc/home_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:fineance/injection/modules.dart';
+import 'package:fineance/repositories/wallet.dart';
 import 'package:fineance/repositories/wallets_repository.dart';
 import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
@@ -12,11 +13,19 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
   final Box walletBox;
 
   HomeBloc(this.walletsRepository, this.walletBox) : super(HomeInitial()) {
-    on<GetNewWallet>((event, emit) {
-      final int _chosenWallet =
-      walletBox.get(CHOSEN_WALLET_INDEX, defaultValue: 0) as int;
+    Init();
 
-      emit(ChosenWallet(chosenWallet: _chosenWallet));
+    on<GetNewWallet>((event, emit) async {
+      await Init();
     });
   }
+
+  Init() async{
+    final int _chosenWallet =
+    walletBox.get(CHOSEN_WALLET_INDEX, defaultValue: 0) as int;
+    await walletsRepository.downloadWallets();
+    Wallet wallet =walletsRepository.wallets[_chosenWallet];
+    emit(ChosenWallet(wallet: wallet));
+  }
+
 }
